662d67fd10795bb1154a3f46e09095049f46971d
Zrokool123 <Zrokool123@gmail.com>
2016-09-01 17:54:30 -0600

    Got it responsive down to 800px

--- Js/myjavascript.js
+++ Js/myjavascript.js
@@ -2,4 +2,16 @@
 
 $(document).ready(function(){
 
+	var $info = $('#info');
+
+	enquire.register("screen and (max-width: 900px)", {
+	    match: function() {
+	        $info.addClass('quick-nav-2');
+	    },
+
+	    unmatch: function() {
+	        $info.removeClass('quick-nav-2');
+	    }
+	}).listen();
+
 });
