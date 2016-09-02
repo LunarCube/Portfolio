fb42b81b4ae8b37d83d23bdbfcf93235869729ab
Zrokool123 <Zrokool123@gmail.com>
2016-09-01 19:22:25 -0600

    wow figured out how to jquery resize and revert back
    couldn't even find a solution to this problem...just thought about it.
    :)
    not sure if it is right, but it works..

--- Js/myjavascript.js
+++ Js/myjavascript.js
@@ -1,17 +1,28 @@
 /* This is where my Js goes*/
 
 $(document).ready(function(){
+	$(window).load(function(){
+		var viewportWidth = $(window).width();
+		if (viewportWidth < 700){
+			$('.logo').removeClass('three columns').addClass('six columns');
+			$('.quick-nav').removeClass('three columns').addClass('four columns');
+			$('.header-cta').removeClass('offset-by-two three columns');
+			$('.nav-btn').removeClass('one column').addClass('two columns');
+		};
+	});
 
-	var $info = $('#info');
-
-	enquire.register("screen and (max-width: 900px)", {
-	    match: function() {
-	        $info.addClass('quick-nav-2');
-	    },
-
-	    unmatch: function() {
-	        $info.removeClass('quick-nav-2');
-	    }
-	}).listen();
-
+	$(window).resize(function(){
+		var viewportWidth = $(window).width();
+		if (viewportWidth < 700){
+			$('.logo').removeClass('three columns').addClass('six columns');
+			$('.quick-nav').removeClass('three columns').addClass('four columns');
+			$('.header-cta').removeClass('offset-by-two three columns');
+			$('.nav-btn').removeClass('one column').addClass('two columns');
+		}else{
+			$('.logo').removeClass('six three columns').addClass('three columns');
+			$('.quick-nav').removeClass('three four columns').addClass('three columns');
+			$('.header-cta').removeClass('offset-by-two three columns').addClass('offset-by-two three columns');
+			$('.nav-btn').removeClass('one two column columns').addClass('one column');
+		};
+	})
 });
