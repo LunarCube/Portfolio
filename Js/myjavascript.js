8fe3f224004257b28a32a8391ddd9ccf9130dac8
Zrokool123 <Zrokool123@gmail.com>
2016-09-04 16:50:36 -0600

    making the landing fully responsive. Figured out the jquery... yes!!!!

--- Js/myjavascript.js
+++ Js/myjavascript.js
@@ -3,26 +3,33 @@
 $(document).ready(function(){
 	$(window).load(function(){
 		var viewportWidth = $(window).width();
-		if (viewportWidth < 700){
+		if (viewportWidth <= 700){
 			$('.logo').removeClass('three columns').addClass('six columns');
 			$('.quick-nav').removeClass('three columns').addClass('four columns');
 			$('.header-cta').removeClass('offset-by-two three columns');
 			$('.nav-btn').removeClass('one column').addClass('two columns');
 		};
+		if (viewportWidth <= 600){
+			$('.logo').removeClass('six columns').addClass('ten columns');
+		};
 	});
+
 
 	$(window).resize(function(){
 		var viewportWidth = $(window).width();
-		if (viewportWidth < 700){
-			$('.logo').removeClass('three columns').addClass('six columns');
-			$('.quick-nav').removeClass('three columns').addClass('four columns');
-			$('.header-cta').removeClass('offset-by-two three columns');
-			$('.nav-btn').removeClass('one column').addClass('two columns');
-		}else{
+		if (viewportWidth >= 700){
 			$('.logo').removeClass('six three columns').addClass('three columns');
 			$('.quick-nav').removeClass('three four columns').addClass('three columns');
 			$('.header-cta').removeClass('offset-by-two three columns').addClass('offset-by-two three columns');
 			$('.nav-btn').removeClass('one two column columns').addClass('one column');
-		};
+			
+		}else if(viewportWidth < 700 && viewportWidth >= 600){
+			$('.logo').removeClass('three ten columns').addClass('six columns');
+			$('.quick-nav').removeClass('three columns').addClass('four columns');
+			$('.header-cta').removeClass('offset-by-two three columns');
+			$('.nav-btn').removeClass('one column').addClass('two columns');
+		}else if(viewportWidth < 600){
+			$('.logo').removeClass('six columns').addClass('ten columns')
+		}
 	})
 });
