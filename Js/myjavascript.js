b0c36990f5d6a52e9dbc66972caf135f75506894
Zrokool123 <Zrokool123@gmail.com>
2016-10-14 13:00:49 -0600

    working. cant get flowtype to work in the slider though...

--- Js/myjavascript.js
+++ Js/myjavascript.js
@@ -133,6 +133,22 @@ $(document).ready(function(){
 	 	fontRatio : 30
 	});
 
+	$('.process-header-text').flowtype({
+		minimum   : 320,
+	 	maximum   : 1600,
+	 	minFont   : 16,
+	 	maxFont   : 34,
+	 	fontRatio : 9
+	});
+
+	$('.process-body-text').flowtype({
+		minimum   : 320,
+	 	maximum   : 1600,
+	 	minFont   : 16,
+	 	maxFont   : 22,
+	 	fontRatio : 10.8
+	});
+
 	$('.contact-header').flowtype({
 		minimum   : 320,
 	 	maximum   : 1600,
